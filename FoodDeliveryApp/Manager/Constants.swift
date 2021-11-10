ee9b5ff82932e1117edd2ab34cb130f209e26e7a
Orlando Vargas <Orlandov87@live.com>
2021-11-09 21:59:16 -0500

    Update Constants.swift

--- FoodDeliveryApp/Manager/Constants.swift
+++ FoodDeliveryApp/Manager/Constants.swift
@@ -17,3 +17,15 @@ let USER_TYPE_DRIVER = "driver"
 
 let STRIPE_PUBLIC_KEY = "pk_test_51HrVG3I0pSVz7qhzEY2QqtUIEExOLgxPUNg6DCif6ioIXwD5bkzGazpkgCr8vxf2CR3ALwgsUCDzArymDdUIZ00E00H73KAHLA"
 
+struct Cosntants {
+    struct EndPoint {
+        static let BASE_URL: String = "https://fooddeliverynowapp.herokuapp.com/"
+    }
+    
+    struct Client {
+        static let ID = "9f1WCfAnSKtS8emLfVydlGicGHal5gNbQY21NgbD"
+        static let SKEY = "evMeguSrXxZddARxWM4wuatDEcH3ufxnnTU6rlX0SiyXQCWgVLrD5PpMIgfA8ObqNFiXVWJxWCEaYniMbHNZdBXE2fg31ZwVYMHlGMDw8xwSDj0OihSGhuxSf37ghgUG"
+    }
+    
+}
+
